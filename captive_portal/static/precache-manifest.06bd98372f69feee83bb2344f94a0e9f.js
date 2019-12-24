06ae9feffe154120af3a27db6640e1944edf2a4a
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2019-12-24 02:36:43 +0100

    Built the webpage!

---
+++ captive_portal/static/precache-manifest.06bd98372f69feee83bb2344f94a0e9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0759ea01b0a949daa29a5dd26397f7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46ded4d58d1506f91c42",
+    "url": "/static/css/2.a0af7764.chunk.css"
+  },
+  {
+    "revision": "6dab12b166d975596885",
+    "url": "/static/css/main.fc68ab2d.chunk.css"
+  },
+  {
+    "revision": "46ded4d58d1506f91c42",
+    "url": "/static/js/2.f8a8cff0.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.f8a8cff0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dab12b166d975596885",
+    "url": "/static/js/main.a7f6886a.chunk.js"
+  },
+  {
+    "revision": "ad1c37562c03924c2abf",
+    "url": "/static/js/runtime-main.db1ddc8a.js"
+  }
+]);
