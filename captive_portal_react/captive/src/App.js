77014daa9dbfffbb179d1f13b5c193e12732ee1f
Pablo Pérez Rodríguez <pablo.rabanales@gmail.com>
2019-12-24 02:26:34 +0100

    New react webpage

--- captive_portal_react/captive/src/App.js
+++ captive_portal_react/captive/src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Form, Input, Button, Icon } from 'antd'
+import { Typography } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Title, Paragraph } = Typography
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      inputCode: '',
+      connected: false,
+      incorrectCode: false,
+      loading: false
+    }
+  }
+
+  handleOk = () => {
+    this.setState({
+      loading: true,
+      incorrectCode: false
+    })
+    fetch('/checkjson', {
+      method: 'POST',
+      body: {
+        code: this.state.inputCode
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.status === 'connected') {
+          this.setState({ loading: false, connected: true })
+        } else {
+          this.setState({
+            loading: false,
+            connected: false,
+            incorrectCode: true
+          })
+        }
+      })
+  }
+
+  render () {
+    if (this.state.connected) {
+      return (
+        <div className='App'>
+          <header className='App-header'>
+            <Title level={2}>"Ojalá esta Wi-Fi fuera fibra"</Title>
+            <Icon type='wifi' style={{ fontSize: '10em', color: '#52c41a' }} />
+            <Paragraph style={{ fontSize: '1.5em', color: '#52c41a' }}>
+              Estás conectado
+            </Paragraph>
+          </header>
+        </div>
+      )
+    }
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <Title level={2}>"Ojalá esta Wi-Fi fuera fibra"</Title>
+          <Paragraph>
+            Introduce el código que está en la pantallita del rúter
+          </Paragraph>
+          <Form.Item
+            validateStatus={this.state.incorrectCode ? 'error' : ''}
+            help={this.state.incorrectCode ? 'Código incorrecto' : ''}
+          >
+            <Input.Group compact size='large' style={{ whiteSpace: 'nowrap' }}>
+              <Input
+                style={{ width: '6em', textAlign: 'center' }}
+                placeholder='Código'
+                maxLength={6}
+                onChange={event =>
+                  this.setState({
+                    inputCode: event.target.value,
+                    incorrectCode: false
+                  })
+                }
+                value={this.state.inputCode}
+                onPressEnter={this.handleOk}
+              />
+              <Button
+                size='large'
+                type='primary'
+                icon='wifi'
+                loading={this.state.loading}
+                onClick={this.handleOk}
+              />
+            </Input.Group>
+          </Form.Item>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
